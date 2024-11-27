7004f23f5d4a7233825801174e21a936f8b8089b
唱唱反调 <58449124+sweetbearbear@users.noreply.github.com>
2024-11-28 02:46:00 +0800

    Create Blued去广告.js

---
+++ QuantumultX/Rewrite/Blued去广告.js
@@ -0,0 +1,8 @@
+# > Blued
+# hostname = social.blued.cn
+# 主页推荐直播 
+^https?:\/\/social\.blued\.cn\/users\/recommend url reject-dict
+# 未登录时个人界面广告
+^https?:\/\/social\.blued\.cn\/users\/no_auth\/benefit url reject-dict
+# 登录后个人界面广告
+^https?:\/\/social\.blued\.cn\/users\/.+\/more\/ios\?v=2 url script-response-body https://raw.githubusercontent.com/fmz200/wool_scripts/main/Scripts/blued.js
